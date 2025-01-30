325a63f2d8f5dab89fe4b8160d248329007aadc6
Александър Бечев <stu2113811001@uni-plovdiv.bg>
2025-01-30 18:06:14 +0200

    Add project files.

---
+++ GamingPeripheralsShop.BL/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using GamingPeripheralsShop.BL.Interfaces;
+using GamingPeripheralsShop.Models.Models.User;
+using GamingPeripheralsShop.DL.Interfaces;
+using GamingPeripheralsShop.Models.Models.Request;
+
+namespace GamingPeripheralsShop.BL.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void Add(Product product)
+        {
+            _productRepository.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productRepository.GetAll();
+        }
+        public Product GetById(int id)
+        {
+            return _productRepository.GetById(id);
+        }
+
+        public void Remove(int id)
+        {
+            _productRepository.Remove(id);
+        }
+
+        public List<Product> GetAllProductsByManufacturerId(int manufacturerId)
+        {
+            return _productRepository.GetAllProductsByManufacturerId(manufacturerId);
+        }
+
+        public int TestCalculation(int number)
+        {
+            var products = _productRepository.GetAll();
+            return products.Count() + number;
+
+        }
+    }
+}
---
+++ GamingPeripheralsShop/Program.cs
@@ -0,0 +1,57 @@
+using GamingPeripheralsShop.DL.Interfaces;
+using GamingPeripheralsShop.DL.Repositories;
+using GamingPeripheralsShop.DL.MemoryDb;
+using GamingPeripheralsShop.BL.Interfaces;
+using GamingPeripheralsShop.BL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using GamingPeripheralsShop.HealthChecks;
+
+namespace GamingPeripheralsShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+            builder.Services.AddSingleton<IManufacturerRepository, ManufacturerRepository>();
+
+            builder.Services.AddSingleton<IProductService, ProductService>();
+            builder.Services.AddSingleton<IManufacturerService, ManufacturerService>();
+            builder.Services.AddSingleton<IStoreService, StoreService>();
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+            builder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
+
+            builder.Services.AddHealthChecks().AddCheck<CustomHealthCheck>(nameof(CustomHealthCheck));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapHealthChecks("/healthz");
+
+            app.Run();
+        }
+    }
+}
